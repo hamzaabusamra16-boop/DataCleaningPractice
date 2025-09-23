de7ebcf6ee922af85edfd4d7544536fbaf959d42
Hamza AbuSamra <hamzaabusamra@Hamzas-MacBook-Air.local>
2025-09-23 04:39:49 +0300

    Add KMeans + Logistic Regression project

--- python/py.py
+++ python/py.py
@@ -1,37 +1,36 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import  LogisticRegression
-from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import KMeans
 import seaborn as sns
-
-def classification(data):
-    data_one = pd.read_csv(data)
-    data_one = data_one.drop(["Unnamed: 32","id"],axis=1)
-    le = LabelEncoder()
-    data_one["diagnosis"] = le.fit_transform(data_one["diagnosis"])
-    x= data_one.drop(["diagnosis"],axis=1)
-    y= data_one["diagnosis"]
-    X_tran , X_test , Y_tran , Y_test = train_test_split(x,y,test_size=0.2,random_state=42,shuffle=True)
-    model = LogisticRegression(max_iter=500,tol=0.01)
-    model.fit(X_tran,Y_tran)
-    print(model.score(X_test,Y_test))
-def lLinear(data):
-    data_two = pd.read_csv(data)
-    data_two = data_two.drop_duplicates()
-    obj_data = data_two.select_dtypes(include="object")
-    num_data = data_two.select_dtypes(exclude="object")
-    le = LabelEncoder()
-    for i in obj_data:
-        obj_data[i] = le.fit_transform(obj_data[i])
-    ready_data = pd.concat([obj_data,num_data],axis=1)
-    model = LinearRegression()
-    x= ready_data.drop(["charges"],axis=1)
-    y= ready_data["charges"]
-    X_tran , X_test , Y_tran , Y_test = train_test_split(x,y,test_size=0.2,random_state=42,shuffle=True)
-    model.fit(X_tran,Y_tran)
-    print(model.score(X_test,Y_test))
-classification("./data/data.csv")
-lLinear("./data/insurance.csv")
+data = pd.read_csv("./data/penguins.csv")
+data = data.dropna()
+le = LabelEncoder()
+data["sex"] = le.fit_transform(data["sex"])
+def draw_Cluster():
+    list = []
+    for i in range(2,31):
+        model= KMeans(n_clusters=i,max_iter=500,random_state=42)
+        model.fit(data)
+        list.append(model.inertia_)
+    plt.plot(range(2,31),list,marker="X")
+    plt.show()
+def Cluster(data):
+    model= KMeans(n_clusters=6,max_iter=500,random_state=42)
+    model.fit(data)
+    data["Cluster"]= model.labels_
+    print(data)
+    def classification(data):
+        x = data.drop(["Cluster"],axis=1)
+        y = data["Cluster"]
+        X_tran, X_test, Y_tran ,Y_test = train_test_split(x,y ,test_size=0.02,random_state=42,shuffle=True)
+        model_tow = LogisticRegression(max_iter=10000,tol=0.01)
+        model_tow.fit(X_tran, Y_tran)
+        print(model_tow.score(X_test, Y_test))
+        plt.hist(y)
+        plt.show()
+    classification(data)    
+Cluster(data)
